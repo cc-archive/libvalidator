2cfbcaf2249c6c8f504535fb4af2d4350d69b143
Hugo Dworak <hugo.dworak+gsoc2008@example.com>
2008-07-07 20:46:33 +0200

    A dummy nosetest to start with

---
+++ tests/test_units/test_triples_extraction.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class TestTriplesExtraction():
+    def __init__(self, *args, **kargs):
+        pass
+    
+    def test_positive(self):
+        pass
+    
+    def test_negative(self):
+        return False
