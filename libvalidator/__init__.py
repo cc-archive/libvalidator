6425c9ce95966b6139f976b09a50e92e3fdd32c9
Hugo Dworak <hugo.dworak+gsoc2008@example.com>
2008-07-08 17:10:30 +0200

    Methods for determining the base URI for both an element and the whole document. No CURIE support as yet.

--- libvalidator/__init__.py
+++ libvalidator/__init__.py
@@ -1,4 +1,46 @@
 # -*- coding: utf-8 -*-
+import re, urlparse
+#from tidy import parseString
+from BeautifulSoup import BeautifulSoup
+
 class libvalidator():
-    def parse(self):
+    def __init__(self, *args, **kargs):
         pass
+    """
+    No CURIE support
+    """
+    def findBaseDocument(self):
+        soup = BeautifulSoup(self.code)
+        element = soup.find('base', {'href': lambda(value): value is not None})
+        reContentLocation = re.compile('(?:^|\s+)Content\-Location\s*:\s*(\S+)', re.IGNORECASE)
+        search = reContentLocation.search(self.headers)
+        if element:
+            self.baseURI = element['href']
+        elif search:
+            self.baseURI = search.group(1)
+        elif location:
+            self.baseURI = self.location
+    def findBaseElement(self, element):
+        if element.name.lower() == 'object':
+            try:
+                return element['codebase']
+            except:
+                pass
+        try:
+            return element['xml:base']
+        except:
+            node = element
+            while node:
+                search = node.findParent(attrs={'xml:base': lambda(value): value is not None})
+                if search:
+                    return search['xml:base']
+                node = node.parent
+        return self.baseURI
+    def parse(self, code, location = None, headers = None):
+        self.code = code
+        if location is not None:
+            self.location = location
+        if headers is not None:
+            self.headers = headers
+        self.findBaseDocument()
+        # incomplete
