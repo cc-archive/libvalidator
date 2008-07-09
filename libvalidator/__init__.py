b98f0e7a3ed6dbc8fb9e723adc41b069d7b59c9a
Hugo Dworak <hugo.dworak+gsoc2008@example.com>
2008-07-10 01:53:39 +0200

    The basis of filtering RDF triples with regard to licensing terms.

--- libvalidator/__init__.py
+++ libvalidator/__init__.py
@@ -14,6 +14,7 @@ class URLOpener(urllib.FancyURLopener):
 class libvalidator():
     def __init__(self, *args, **kargs):
         self.reRDF = re.compile('<([^\s<>]+)\s+(?:[^>]+\s+)?xmlns(?::[^=]+)?\s*=\s*(?:("http://www\.w3\.org/1999/02/22-rdf-syntax-ns#")|(\'http://www\.w3\.org/1999/02/22\-rdf\-syntax\-ns#\')).*</\\1\s*>')
+        self.result = []
     """
     No CURIE support
     Should be rewritten to utilise DOM instead of the BeautifulSoup parsing
@@ -48,18 +49,28 @@ class libvalidator():
                 node = node.parent
         return self.baseURI
     """
-    def parse(self, code, location = None, headers = None):
-        if location is not None:
-            self.location = location
-        if headers is not None:
-            self.headers = headers
+    def canonicalization(self, code):
         try:
             dom = minidom.parseString(code)
         except ExpatError, err:
             code = str(BeautifulSoup(code))
             # the following can raise ExpatError again
             dom = minidom.parseString(code)
-        self.code = c14n.Canonicalize(dom, comments=True)
+        return c14n.Canonicalize(dom, comments=True)
+    def extractLicensedObjects(self, code, base):
+        code = self.canonicalization(code)
+        graph = rdflib.ConjunctiveGraph()
+        graph.parse(rdflib.StringInputSource(code))
+        # rdf:sameAs for xhv:license and DC.rights.license
+        for row in graph.query('SELECT ?a ?b WHERE { ?a cc:license ?b . }', initNs=dict(cc=rdflib.Namespace('http://web.resource.org/cc/'))):
+            print '---License information---'
+            print "%s is licensed under %s" % row
+    def parse(self, code, location = None, headers = None):
+        if location is not None:
+            self.location = location
+        if headers is not None:
+            self.headers = headers
+        self.code = self.canonicalization(code)
         self.dom = minidom.parseString(self.code)
         self.findBaseDocument()
         sources = []
@@ -67,13 +78,18 @@ class libvalidator():
             sources.append([self.baseURI, m.group(0)])
         n3 = _process_DOM(self.dom, self.location, 'n3', Options(warnings=False))
         graph = rdflib.ConjunctiveGraph()
-        graph.parse(rdflib.StringInputSource(nt), format='n3')
-        for row in graph.query('SELECT ?b WHERE { ?a xhv:meta ?b . }', initNs=dict(xhv=rdflib.Namespace("http://www.w3.org/1999/xhtml/vocab#"))):
+        graph.parse(rdflib.StringInputSource(n3), format='n3')
+        sources.append([self.baseURI, graph.serialize(format='xml')])
+        for row in graph.query('SELECT ?b WHERE { ?a xhv:meta ?b . }', initNs=dict(xhv=rdflib.Namespace('http://www.w3.org/1999/xhtml/vocab#'))):
             try:
                 f = URLOpener().open(row[0])
+                # FIXME detect MIME type (we assume it must be application/rdf+xml as yet)
                 if not unicode(row[0]).startswith('data:'):
                     sources.append([str(row[0]), f.read()])
                 else:
                     sources.append([self.baseURI, f.read()])
             except IOError:
                 pass
+        for (base, source) in sources:
+            self.extractLicensedObjects(source, base)
+        # aside of the licensed objects, gather information about licenses themselves
