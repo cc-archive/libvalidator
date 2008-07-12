7bff90c34e1b9e44a6658b41fc96ff6da19a0777
Hugo Dworak <hugo.dworak+gsoc2008@example.com>
2008-07-12 20:29:30 +0200

    Enhanced dependencies and metadata.

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='libvalidator',
     version='0.1',
-    #description='',
-    #author='',
+    description='A library that parses (X)HTML for RDF with regard to licensing.',
+    author='Hugo Dworak',
     #author_email='',
     #url='',
     install_requires=['setuptools',
                       'nose',
+                      'html5lib',
+                      'rdflib',
+                      'BeautifulSoup',
+                      'pyRdfa'
                       ],
     packages=find_packages(exclude=['ez_setup',]),
     include_package_data=True,
